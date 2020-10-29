01be6c4bf8dcf503093754eef745ee6f1b80b910
hun <mucss@naver.com>
2020-10-29 11:15:02 +0900

    개인정보보호

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyparser = require('body-parser');
+const config = require('./config/key');
+const {User} = require('./models/User');
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hun:gnstkd159@hunsang.wwqmp.mongodb.net/hunsang?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=> console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
 
+app.post('/register', (req,res) => {
+  
+  const user = new User(req.body);
+
+  user.save((err,userInfo) => {
+    if (err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요 예스')
 })
 
 app.listen(port, () => {
